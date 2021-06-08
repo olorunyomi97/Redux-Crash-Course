304c69a4b7d74386f45dd4b8fa221389720fac5b
olorunyomi97 <olorunyomi97@gmail.com>
2021-06-08 12:19:32 +0100

    Redux Crash Course

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import Posts from './components/Posts';
+import Postform from './components/Postform';
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
